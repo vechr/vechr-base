e24d61937ec7d6ca7c504bed2a8cdca32ba12cb5
zulfikar4568 <isnaen70@gmail.com>
2025-04-30 20:19:54 +0700

    fix: delete re-export nestjs

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ export * from './infrastructure';
 export * from './tracing';
 
 // Re-export commonly used NestJS components
-export * from '@nestjs/common';
-export * from '@nestjs/core';
-export * from '@nestjs/platform-express';
-export * from '@nestjs/cache-manager';
-export * from '@nestjs/common';
-export * from '@nestjs/core';
-export * from '@nestjs/jwt';
-export * from '@nestjs/microservices';
-export * from '@nestjs/swagger';
-export * from '@nestjs/terminus';
+// export * from '@nestjs/common';
+// export * from '@nestjs/core';
+// export * from '@nestjs/platform-express';
+// export * from '@nestjs/cache-manager';
+// export * from '@nestjs/common';
+// export * from '@nestjs/core';
+// export * from '@nestjs/jwt';
+// export * from '@nestjs/microservices';
+// export * from '@nestjs/swagger';
+// export * from '@nestjs/terminus';
