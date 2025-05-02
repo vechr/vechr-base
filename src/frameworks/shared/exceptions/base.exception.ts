a8a39cd3e9e0ee69c3c859b2094fa10f35f25a46
zulfikar4568 <isnaen70@gmail.com>
2025-05-03 00:33:18 +0700

    fix: add comment for base exception

--- src/frameworks/shared/exceptions/base.exception.ts
+++ src/frameworks/shared/exceptions/base.exception.ts
@@ -1,7 +1,17 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 import { IErrorResponse } from '../responses/error.response';
-
+/**
+ * BaseException is a custom exception class extending NestJS's HttpException.
+ * It allows attaching a custom error code and additional parameters to the error response.
+ */
 export class BaseException extends HttpException {
+  /**
+   * Creates a new instance of BaseException.
+   * @param httpStatus - The HTTP status code to be returned.
+   * @param code - A custom application-specific error code.
+   * @param message - A human-readable error message.
+   * @param params - Optional additional parameters relevant to the error.
+   */
   constructor(
     public httpStatus: HttpStatus,
     private code: string,
